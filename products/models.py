8a369312f40abe53cf8aad1c74c9d9c091df68f4
Sudipto Baral <sudipto.baral@bracits.com>
2022-04-08 13:48:35 +0600

    image size increased

--- products/models.py
+++ products/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=20000)
+    image = models.TextField(max_length=20000000)
     likes = models.PositiveIntegerField(default=0)
