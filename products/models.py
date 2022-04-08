8e40dc8e26e415e87b03223f2500851f6a2dfd04
Sudipto Baral <sudipto.baral@bracits.com>
2022-04-08 13:42:28 +0600

    image co

--- products/models.py
+++ products/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
-    image = models.CharField(max_length=200)
+    image = models.CharField(max_length=20000)
     likes = models.PositiveIntegerField(default=0)
