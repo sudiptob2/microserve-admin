3424278c4148770883814f3c6e842eef61171f8c
Sudipto Baral <sudipto.baral@bracits.com>
2022-04-11 18:52:21 +0600

    consumer exception handled

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 
 import django
 import pika
@@ -11,21 +12,12 @@ from products.models import Product
 
 RABBIT_ENDPOINT = os.environ['RABBIT_ENDPOINT']
 
-params = pika.URLParameters(RABBIT_ENDPOINT)
-
-connection = pika.BlockingConnection(params)
-
-channel = connection.channel()
-
-channel.queue_declare(queue='admin')
-
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    id = json.loads(body)
-    print(id)
+    product_id = json.loads(body)
     product = Product.objects.filter(
-        id=id,
+        id=product_id,
     ).first()
     if product:
         product.likes = product.likes + 1
@@ -33,10 +25,32 @@ def callback(ch, method, properties, body):
         print('Product likes increased!')
 
 
-channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+class Consumer:
+    """Implement the consumer script."""
 
-print('Started Consuming')
+    def start(self, connection):
+        # if not self.connection or self.connection.is_closed:
+        #     self.connection = pika.BlockingConnection(self.params)
+        channel = connection.channel()
+        channel.queue_declare(queue='admin')
+        channel.basic_consume(
+            queue='admin',
+            on_message_callback=callback,
+            auto_ack=True,
+        )
+        print('Started Consuming in admin...')
+        channel.start_consuming()
+        channel.close()
 
-channel.start_consuming()
 
-channel.close()
+print('Starting the Queue.....')
+consumer = Consumer()
+params = pika.URLParameters(RABBIT_ENDPOINT)
+while True:
+    try:
+        connection = pika.BlockingConnection(params)
+    except:
+        print('could not connect to the host. retrying in 1 sec...')
+        time.sleep(1)
+        continue
+    consumer.start(connection)
